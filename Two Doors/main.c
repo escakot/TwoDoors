24014f0dabe170b543e90cdeee6e5884dd15619b
escakot <ec@neorennie.com>
2017-05-22 17:52:21 -0400

    Initial Commit

---
+++ Two Doors/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Two Doors
+//
+//  Created by Errol Cheong on 2017-05-22.
+//  Copyright © 2017 Errol Cheong. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
